e276c7535fdf231ca5baa619f8b7d94d481bc18a
qinhj <qinhongjie@imilab.com>
2020-02-27 18:11:39 +0800

    optimize with color

--- template.sh
+++ template.sh
@@ -5,55 +5,99 @@
 # ==================================================================
 # @Author:  qinhj@lsec.cc.ac.cn
 # ------------------------------------------------------------------
-# @Note:    To use getopts, we need bash.
+# @Note:    To use "getopts" "let", we need bash.
 # ------------------------------------------------------------------
 # @History:
-# 2020/02/19 v0.0.0 init
+# 2020/02/27 v0.1.0 optimize(color, ...)
+# 2020/02/19 v0.0.0 init/create
 ####################################################################
 
+#########################
 #### Global Settings ####
+#########################
 
 ## debug settings
-#set -x
+PATH=.:$PATH
+set -e # -ex
 ## script version
-VERSION=0.0.0
+VERSION=0.1.0
 ## script options
-COMMAND="usage"
-OPTIONS=":vs:"
-COMMENT="<command> [-v] [-s <string>]"
-EXAMPLE="test -v -s 'hello world'"
+COMMANDS="usage test"
+COMMENTS="<command> [options]"
+OPTIONS=":s:"
+EXAMPLE0="usage"
+EXAMPLE1="test -s 'hello world'"
 ## other variable
+CONFIGS=
 MESSAGE="hello world"
 
+#########################
 #### Function Region ####
+#########################
 
-Usage() {
-    echo "Shell Script Template [Version: $VERSION]"
-    echo "Usage:"
-    echo "  $0 $COMMENT"
-    echo "Commands:"
-    echo "  usage   show usage"
-    echo "  test    quick test"
-    echo "Options:"
-    echo "  -v      show version"
-    echo "  -s      demo string"
-    echo "Examples:"
-    echo "  $0 $EXAMPLE"
+### Private  Function ###
+
+## hard code as Red
+EchoError() {
+  echo -e "\033[31m[Error ] $@\033[0m"
 }
 
-Test() {
-    echo "[ Test ] Start."
-    echo "[ Test ] $MESSAGE"
-    echo "[ Test ] Finish."
+## hard code as LightBlue
+EchoTitle() {
+  echo -e "\033[36m$@\033[0m"
 }
 
+## hard code as Yellow
+EchoSection() {
+  echo -e "\033[33m$@\033[0m"
+}
+
+ShowUsage() {
+  EchoSection "Usage:"
+  echo "  $0 $COMMENTS"
+}
+
+ShowCommand() {
+  EchoSection "Command:"
+  echo "  usage show usage"
+  echo "  test  quick test"
+}
+
+ShowOptions() {
+  EchoSection "Options:"
+  echo "  -s    demo string (default: $MESSAGE)"
+}
+
+ShowExample() {
+  EchoSection "Example:"
+  echo "  $0 $EXAMPLE0"
+  echo "  $0 $EXAMPLE1"
+}
+
+#### Public Function ####
+
+usage() {
+  EchoTitle "Shell Script Template [Version: $VERSION]"
+  ShowUsage
+  ShowCommand
+  ShowOptions
+  ShowExample
+}
+
+test() {
+  echo "[ Test ] Start: $@"
+  echo "[ Test ] $MESSAGE"
+  echo "[ Test ] Finish."
+}
+
+#########################
 ####  Main   Region  ####
+#########################
 
 ## check inputs
 if [ 0 -eq $# ]; then
-    echo "[Error ] Missing necessary arguments!"
-    Usage
-    exit 0
+  usage
+  exit 0
 fi
 
 ## get command
@@ -63,30 +107,26 @@ shift
 
 ## parse option
 while getopts $OPTIONS opt; do
-    #echo "[Debug ] opt: $opt, arg: $OPTARG"
-    case $opt in
-        v)
-            echo "$0 Version: $VERSION";;
-        s)
-            MESSAGE=$OPTARG
-            echo "[Debug ] get message: $MESSAGE";;
-        ?)
-            echo "[Error ] Unknown option: $OPTARG"
-            break
-    esac
+  #echo "[Debug ] opt: $opt, arg: $OPTARG"
+  case $opt in
+    s)
+      MESSAGE=$OPTARG
+      echo "[Debug ] get message: $MESSAGE";;
+    ?)
+      EchoError "unknown option: $OPTARG"
+      break
+  esac
 done
-## option debug
 #echo "[Debug ] opt: $@, optind: $OPTIND"
-#shift $(($OPTIND - 1))
+
+## shift to remain options
+shift $(($OPTIND - 1))
 #echo "[Debug ] remain opt: $@"
 
 ## run command
-case $COMMAND in
-    usage)
-        Usage;;
-    test)
-        Test;;
-    *)
-        echo "[Error ] Unknown Command: $COMMAND"
-        Usage;;
-esac
+if [[ $COMMANDS =~ $COMMAND ]]; then
+  $COMMAND $@
+else
+  EchoError "Unknown Command: $COMMAND"
+  usage
+fi
